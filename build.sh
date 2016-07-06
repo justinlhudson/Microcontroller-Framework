ce3d65efea176643e38e56f2a926385c6c1f2e78
Justin L Hudson <justinlhudson@gmail.com>
2016-07-06 16:40:38 -0400

    cleanup

--- build.sh
+++ build.sh
@@ -3,9 +3,14 @@
 set -e
 
 # Configure
-_name=dev
+_name=development
 _baud=115200
-_port=/dev/tty.usbmodem1411
+
+if [ ! -z "$2" ]; then
+  _port="$2"
+else
+  _port=/dev/tty.usbmodem1411
+fi
 
 if [ ! -z "$1" ] && [[ "$1" == "-p" ]]; then
   avrdude -F -D -c stk500v2 -p m2560 -b $_baud -P $_port -U flash:w:$_name.hex -U eeprom:w:$_name.eep
