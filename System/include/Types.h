6be410df9a2fda1c3ccb580b5bdf11e7e8b6da39
Justin L Hudson <justinlhudson@gmail.com>
2018-02-08 17:44:53 -0500

    not needed for some reason, but commented out for reference

--- System/include/Types.h
+++ System/include/Types.h
@@ -153,25 +153,21 @@ typedef volatile uint8_t     reg8;
   // this is applicable if using virtual inheritance.
   __extension__ typedef int __guard __attribute__((mode (__DI__)));
 
+  extern "C" int __cxa_guard_acquire(__guard *);
+  extern "C" void __cxa_guard_release (__guard *);
+  extern "C" void __cxa_guard_abort (__guard *);
+
   int __cxa_guard_acquire(__guard *g) {return !*(char *)(g);};
   void __cxa_guard_release (__guard *g) {*(char *)g = 1;};
   void __cxa_guard_abort (__guard *) {};
 
   // this is applicable if using pure virtual inheritance.
+  extern "C" void __cxa_pure_virtual(void);
+
   void __cxa_pure_virtual(void) {};
 */
 #else
-  /*
-    // this is applicable if using virtual inheritance.
-  __extension__ typedef int __guard __attribute__((mode (__DI__)));
 
-  extern "C" int __cxa_guard_acquire(__guard *);
-  extern "C" void __cxa_guard_release (__guard *);
-  extern "C" void __cxa_guard_abort (__guard *);
-
-  // this is applicable if using pure virtual inheritance.
-  extern "C" void __cxa_pure_virtual(void);
-  */
 #endif
 
 #endif
